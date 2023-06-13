d3b9b8a7e26f78911e1f70e19462c72a8fe87ce1
Rufat Aliyev <rufat845@gmail.com>
2023-06-13 15:18:22 +0400

    her

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Hero.module.scss";
 export default function Hero({}) {
   return (
     <section className={styles.hero} id="hero">
-      s
+       
     </section>
   );
 }
